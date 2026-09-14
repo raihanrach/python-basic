cc358c5766206e367bf695fa0d0c45fe3bf72f18
Raihan Rachmadani <90181208@hq.bri.co.id>
2026-09-14 10:55:57 +0700

    Training Python Basic

---
+++ train-data-engineer.py
@@ -0,0 +1,38 @@
+# name = "Raihan Rachmadani"
+# job = "Data Engineer"
+# city = "Jakarta"
+
+# umur = 26
+# gaji = "coba tebak"
+# kerja = "BRI"
+# total_pekerja = "Total pekerja di BRI banyak sekali"
+# tim = "Diabloers"
+
+# print (f"Halo namaku adalah : {name}, pekerjaanku sebagai {job} dan aku tinggal di kota {city}")
+# print (f"Umurku adalah {umur}, gaji ku {gaji} dan aku bekerja di bank {kerja}, total pekerja di bank {kerja} adalah {total_pekerja}")
+# print (f"Aku juga merupakan bagian dari tim {tim}")
+# # print ("data pertama")
+
+
+
+# desc = "Belajar aritmatika dasar di python"
+# text = "8 "
+# a=8
+# b=2 
+
+# print (f"{desc}")
+# print (a + b)
+# print (a - b)
+# print (a * b)
+# print (a / b)
+# print (a // b)
+# print (a % b)
+# print (a ** b)
+
+
+lapar = "makan"
+tidak_lapar = "tidak makan"
+if tidak_lapar:
+    print(f"{lapar}")
+else:
+    print(f"{tidak_lapar}")
